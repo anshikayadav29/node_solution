9c01d23de732bbbe085f49bf1251014286a89952
anshikayadav11 <anshikayadav2917@gmail.com>
2025-12-29 11:13:49 +0530

    commit

---
+++ Basic.js
@@ -0,0 +1,28 @@
+/*var a=10;
+let b=20;
+const c=30;
+
+b=100;
+a=200;
+console.log(a+b+c); */
+
+let b=20;
+const c=30;
+
+b=100;
+let a=200;
+
+console.log(a+b+c);
+if(a==200){
+    console.log("this is if condition");
+
+}
+else{
+    console.log("This is wrong condition")
+
+}
+function fruit(item){
+    console.log("this is "+item);
+
+}
+console.log("banana");
