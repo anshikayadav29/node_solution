154607fd3db03a325507ca6b5f7733d9b4f89cc7
anshikayadav11 <anshikayadav2917@gmail.com>
2026-01-01 18:14:23 +0530

    commit

--- Server.js
+++ Server.js
@@ -1,7 +1,16 @@
-const http = require('http');
+/*const http = require('http');
 
 http.createServer((req, resp) => {
     resp.end("hello");
 }).listen(3000, () => {
     console.log("Server running on http://localhost:3000");
+});
+*/
+
+const http = require('http');
+
+http.createServer((req, resp) => {
+    resp.end("hello");
+}).listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
 });
