2e1dc07fc834c677e4a06f39e498212a2b428fae
anshikayadav11 <anshikayadav2917@gmail.com>
2026-01-06 10:48:03 +0530

    commit

---
+++ switchrouting.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.end("Home");
+      break;
+    case "/about":
+      res.end("About Page");
+      break;
+    default:
+      res.end("404 Page");
+  }
+}).listen(5000);
