de8eae142790bbf38a3815b560afd3739f650058
anshikayadav29 <anshikayadav2917@gmail.com>
2026-02-09 00:01:40 +0530

    Create reqbodysizeguard.js

---
+++ reqbodysizeguard.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  let size = 0;
+  req.on("data", chunk => {
+    size += chunk.length;
+    if (size > 1024) req.destroy();
+  });
+  req.on("end", () => res.end("OK"));
+}).listen(5000);
