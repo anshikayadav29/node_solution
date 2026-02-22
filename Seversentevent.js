2c70b09fbdc2dc5aacaed95ac965eb646a5b0ef2
anshikayadav29 <anshikayadav2917@gmail.com>
2026-02-23 00:09:50 +0530

    Create Seversentevent.js

---
+++ Seversentevent.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http.createServer((req,res)=>{
+  res.writeHead(200,{
+    "Content-Type":"text/event-stream",
+    "Cache-Control":"no-cache"
+  });
+  let i=0;
+  setInterval(()=>res.write(`data: tick ${i++}\n\n`),1000);
+}).listen(5000);
